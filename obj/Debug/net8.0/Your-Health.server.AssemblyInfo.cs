ef8ec98cc20f2f85e32ad906db48fac84151bf35
Hatem Aiman Fadl <hatemaimanfadl333@gmail.com>
2024-07-28 15:15:36 +0300

    Initializing App
    Initailaizing App with the EF data base model and all its srvices and CRUD controllers

--- obj/Debug/net8.0/Your-Health.server.AssemblyInfo.cs
+++ obj/Debug/net8.0/Your-Health.server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Your-Health.server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a11d013af26ca6fc22eaac667df23a10e51fff33")]
 [assembly: System.Reflection.AssemblyProductAttribute("Your-Health.server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Your-Health.server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
